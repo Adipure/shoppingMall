0065f60ba1d6c3041004420dc756880c5f327aa0
adipure <chanwoo1017@hotmail.com>
2022-07-28 02:11:24 -0700

    working on nav and carousel

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,20 +1,24 @@
+/* eslint-disable react/style-prop-object */
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
 import './App.css'
+import MainCarousel from './components/MainCarousel'
 
 function App () {
   return (
     <div className='App'>
       <Navbar className='black-nav'>
         <Container>
-          <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+          <Navbar.Brand href='#home'>AWOS Style</Navbar.Brand>
           <Nav className='me-auto'>
             <Nav.Link href='#home'>Home</Nav.Link>
             <Nav.Link href='#features'>Features</Nav.Link>
-            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+            <Nav.Link href='#newarrived'>New Arrived</Nav.Link>
+            <Nav.Link href='#cart'>Cart</Nav.Link>
+            <Nav.Link href='#checkout'>Check Out</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
+      <MainCarousel />
     </div>
   )
 }
